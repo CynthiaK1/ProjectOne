b4c71d356e986e53de8fdfd5c0b59cc8fb1e104c
CynthiaK1 <109212810+CynthiaK1@users.noreply.github.com>
2022-10-20 22:34:46 +0300

    Add files via upload

---
+++ registration.sql
@@ -0,0 +1,127 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:4306
+-- Generation Time: Jul 17, 2022 at 03:15 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.2.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `registration`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `name`, `price`) VALUES
+(1, 'Cannon EOS', 36000),
+(2, 'Sony DSLR', 40000),
+(3, 'Sony DSLR', 50000),
+(4, 'Olympus DSLR', 80000),
+(5, 'Titan Model #301', 13000),
+(6, 'Titan Model #201', 3000),
+(7, 'HMT Milan', 8000),
+(8, 'Favre Lueba #111', 18000),
+(9, 'Raymond', 1500),
+(10, 'Charles', 1000),
+(11, 'HXR', 900),
+(12, 'PINK', 1200);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(1, 'mary', 'maryaladaw@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b'),
+(2, 'admin', 'admin@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_items`
+--
+
+CREATE TABLE `users_items` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `status` enum('Added to cart','Confirmed') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users_items`
+--
+
+INSERT INTO `users_items` (`id`, `user_id`, `item_id`, `status`) VALUES
+(7, 3, 3, 'Added to cart'),
+(8, 3, 4, 'Added to cart'),
+(9, 3, 5, 'Added to cart'),
+(10, 3, 11, 'Added to cart'),
+(11, 1, 9, 'Added to cart'),
+(12, 1, 2, 'Added to cart'),
+(13, 1, 8, 'Added to cart'),
+(14, 4, 2, 'Confirmed'),
+(18, 5, 11, 'Added to cart'),
+(20, 5, 5, 'Added to cart');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
